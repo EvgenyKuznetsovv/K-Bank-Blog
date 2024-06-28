c5a41123d543cebb95464c3c1349c1fbc247fe3e
EvgenyKuznetsov153502 <15350071@study.bsuir.by>
2024-06-28 23:44:24 +0300

    creating authorization, adding middleware and code refactoring

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import express from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from "bcrypt";
 import mongoose from 'mongoose';
-import { registerValidation } from "./validations/auth.js";
-import { validationResult } from "express-validator"; 
-import UserModel from "./models/User.js";
+import { registerValidation, loginValidation } from "./validations.js";
+import checkAuth  from "./utils/checkAuth.js";
 
+import * as UserController from "./controllers/UserController.js"
 
 mongoose
     .connect('mongodb+srv://eugene:wwwwww@cluster0.vw32mqv.mongodb.net/kbank?retryWrites=true&w=majority&appName=Cluster0')
@@ -21,50 +19,9 @@ app.get('/', (req, res) => {
     res.send('hello world');
 });
 
-app.post("/auth/register", registerValidation, async (req, res) => {
-    try {
-        const errors = validationResult(req);
-
-        if(!errors.isEmpty()){
-            return res.status(400).json(errors.array());
-        }
-
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
-
-        const doc = new UserModel({
-		    email: req.body.email,
-		    fullName: req.body.fullName,
-		    avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-	    });
-
-        const user = await doc.save();
-
-        const token = jwt.sign(
-        {
-            _id: user._id,
-        },
-        "secretKey",
-        {
-            expiresIn: '30d',
-        }, 
-        );
-
-        const { passwordHash, ...userData } = user._doc;
-
-        res.json({
-            ...userData,
-            token,
-        });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            message: 'Не удалось зарегистрироваться'
-        });
-    }
-});
+app.post("/auth/register", registerValidation, UserController.register);
+app.post("/auth/login", loginValidation, UserController.login);
+app.get("/auth/me", checkAuth, UserController.getMe)
 
 app.listen(4444, (err) => {
     if(err) {
--- validations.js
+++ validations.js
@@ -6,3 +6,9 @@ export const registerValidation = [
     body('fullName', "Слишком короткое имя").isLength({ min: 3 }),
     body('avatarUrl', "Неверная ссылка на аватарку").optional().isURL(),
 ]
+
+export const loginValidation = [
+	body('email', 'Неверный формат почты').isEmail(),
+	body('password', "Пароль должен содержать минимум 5 символов").isLength({ min: 5 }),
+]
+
