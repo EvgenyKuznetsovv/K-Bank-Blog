3154521ceb105d436fdb86d5fcab400f5f52634b
EvgenyKuznetsov153502 <15350071@study.bsuir.by>
2024-06-29 01:19:12 +0300

    creating Post model, adding validation and controller for post create

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import { registerValidation, loginValidation } from "./validations.js";
+import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
 import checkAuth  from "./utils/checkAuth.js";
 
 import * as UserController from "./controllers/UserController.js"
+import * as PostController from "./controllers/PostController.js"
 
 mongoose
     .connect('mongodb+srv://eugene:wwwwww@cluster0.vw32mqv.mongodb.net/kbank?retryWrites=true&w=majority&appName=Cluster0')
@@ -21,7 +22,13 @@ app.get('/', (req, res) => {
 
 app.post("/auth/register", registerValidation, UserController.register);
 app.post("/auth/login", loginValidation, UserController.login);
-app.get("/auth/me", checkAuth, UserController.getMe)
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+// app.get("/posts", PostController.getAll);
+// app.get("posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+// app.delete("/posts", PostController.remove);
+// app.patch("/posts", PostController.update);
 
 app.listen(4444, (err) => {
     if(err) {
--- validations.js
+++ validations.js
@@ -12,3 +12,9 @@ export const loginValidation = [
 	body('password', "Пароль должен содержать минимум 5 символов").isLength({ min: 5 }),
 ]
 
+export const postCreateValidation = [
+    body('title', 'Длина заголовка минимум 3 символа (формат: строка)').isLength({ min: 3 }).isString(),
+    body('text', 'Длина текста минимум 10 символов (формат: строка)').isLength({ min: 10 }).isString(),
+    body('tags', 'Неверный формат заголовка').optional().isString(),
+    body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+]
