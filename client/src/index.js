438ba2fed080f679b9d70116670396a76938a82f
EvgenyKuznetsov153502 <15350071@study.bsuir.by>
2024-07-02 01:31:26 +0300

    adding redux, axios, routs and cors

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
+import { Provider } from 'react-redux';
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -7,13 +9,19 @@ import "./index.scss";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./theme";
 
+import store from "./redux/Store";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <React.StrictMode>
+  <>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
     </ThemeProvider>
-  </React.StrictMode>
+  </>
 );
